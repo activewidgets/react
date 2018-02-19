9b206a58216f9179989a45c9cb580cb0d8d84abc
alex <alex@activewidgets.com>
2018-02-19 18:48:15 +0100

    add build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import {ReactBuilder} from '@activewidgets/frameworks';
 import {grid, row} from '@activewidgets/grid';
-import '@activewidgets/grid/index.css'
 
 const build = ReactBuilder.init(React.createElement, React.PureComponent);
 
