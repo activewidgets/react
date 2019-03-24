06236b19b9e31cd4259b0d2628729198e98d3065
alex <alex@activewidgets.com>
2019-03-24 18:26:05 +0100

    remove Template class

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import {grid, row} from '@activewidgets/grid';
 
 const build = ReactBuilder.init(React.createElement, React.PureComponent);
 
-export const Template = ReactBuilder.Template;
-
 export const Grid = build(grid);
 export const Row = build(row);
 
 export const components = {Grid, Row};
 
-export default {Template, components, Grid, Row};
+export default {components, Grid, Row};
