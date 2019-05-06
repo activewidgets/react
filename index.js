db8b4d523ab7553fe9b45a5606dca0c7697320fc
alex <alex@activewidgets.com>
2019-05-06 23:29:32 +0200

    use build()

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 
 import React from 'react';
 import framework from '@activewidgets/frameworks/react';
-import {grid, row} from '@activewidgets/grid';
+import * as pkg from '@activewidgets/grid';
 
-let {component} = framework(React);
+const {build} = framework(React);
 
-export const Grid = component(grid);
-export const Row = component(row);
-
-export const components = {Grid, Row};
-
+export const {Grid, Row, View, Scrollbars} = build(pkg);
