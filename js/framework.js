5f0c40aec693f88e6f178e06fecef084e1a745ac
alex <alex@activewidgets.com>
2019-12-18 10:20:17 +0100

    split js/index into framework, components

---
+++ js/framework.js
@@ -0,0 +1,5 @@
+
+import adapter from '@activewidgets/frameworks/react';
+import React from 'react';
+
+export const {build} = adapter(React);
--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
 
-import adapter from '@activewidgets/frameworks/react';
-import * as sources from '@activewidgets/datagrid/js';
-import React from 'react';
-
-export const {build} = adapter(React);
-export const {Datagrid, Row, Gridview, Scrollbars} = build(sources);
+export * from './framework.js';
+export * from './datagrid.js';
