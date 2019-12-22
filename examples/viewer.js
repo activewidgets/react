3cc34969279feb0b090c0bf03b7603362205a2e0
alex <alex@activewidgets.com>
2019-12-22 22:04:34 +0100

    examples viewer

---
+++ examples/viewer.js
@@ -0,0 +1,56 @@
+﻿
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as components from '../';
+import * as pages from './index.js';
+
+
+let el = document.querySelector('#app'),
+    lookup = {},
+    links = [],
+    key = 1;
+
+
+function path(s){
+    return '/' + s.replace(/^\W+|\W+$/g, '').replace(/\W+/g, '-').toLowerCase();
+}
+
+
+Object.keys(pages).forEach(section => {
+    links.push(`<div class="menu-header">${section}</div>`);
+    Object.keys(pages[section]).forEach(name => {
+        let url = path(section) + path(name);
+        lookup[url] = pages[section][name];
+        links.push(`<div class="menu-item"><a href="#view${url}">${name}</a></div>`);
+    });
+});
+
+
+function index(name){
+    ReactDOM.unmountComponentAtNode(el);
+    el.innerHTML = name ? 'not found ' + name : links.join('');
+}
+
+
+function render(Component, props){
+    props.key = ++key;
+    ReactDOM.render(React.createElement(Component, props), el);
+}
+
+
+function refresh(){
+
+    let name = location.hash.replace(/^#(\w+)/, ''),
+        mode = RegExp.$1;
+
+    if (name && typeof lookup[name] == 'function') {
+        lookup[name]({render}, components, {});
+    }
+    else {
+        index(name);
+    }
+}
+
+
+window.addEventListener("hashchange", refresh, false);
+refresh();
