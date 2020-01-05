64f437353af7a42302762f2427bcd8c558b28417
alex <alex@activewidgets.com>
2020-01-05 18:06:45 +0100

    add example

---
+++ examples/get-started/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Datagrid} from "@activewidgets/react";
+import {columns, rows} from "./data.js"
+import "./styles.css";
+
+function App() {
+  return (
+      <Datagrid columns={columns} rows={rows} />
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
