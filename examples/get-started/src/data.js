8b90ec17f2d66dc889331ed2d4c5cf1c45761f78
alex <alex@activewidgets.com>
2020-01-15 18:22:12 +0100

    fix examples columns

--- examples/get-started/src/data.js
+++ examples/get-started/src/data.js
@@ -1,15 +1,15 @@
 
 export const columns = [
-  {header: 'Code', field: 'CustomerID', width: 80, background: '#def', fixed: true},
+  {header: 'Code', field: 'CustomerID', width: 80, style: 'background:#def', fixed: true},
   {header: 'Company Name', field: 'CompanyName', width: 160, fixed: true},
   {header: 'Contact', field: 'ContactName', width: 120},
-  {header: 'Title', field: 'ContactTitle', width: 120, background: '#fed'},
+  {header: 'Title', field: 'ContactTitle', width: 120, style: 'background:#fed'},
   {header: 'Address', field: 'Address', width: 120},
   {header: 'City', field: 'City'},
   {header: 'Zip', field: 'PostalCode', align: 'right'},
   {header: 'Phone', field: 'Phone'},
   {header: 'Fax', field: 'Fax'},
-  {header: 'Country', field: 'Country', background: '#efe', fixed: true}
+  {header: 'Country', field: 'Country', style: 'background:#efe', fixed: true}
 ];
 
 export const rows = [
