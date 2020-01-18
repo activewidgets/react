8ba3eff569c98387040cdc093bb6cc2da388739f
alex <alex@activewidgets.com>
2020-01-19 00:12:03 +0100

    use component tags in tests

--- test/adapter/react.js
+++ test/adapter/react.js
@@ -1,10 +1,21 @@
 
 import React from 'react';
 import {render as originalRender, fireEvent, wait, waitForElement} from '@testing-library/react';
+import * as components from '@activewidgets/components';
 
+let tags = {};
+
+Object.keys(components).forEach(name => {
+    tags['ax-' + name.toLowerCase()] = components[name];
+});
 
 export function render(component, props){
-    return originalRender(React.createElement(component, props));
+
+    if (!tags[component]){
+        throw new Error('component not found - ' + component);
+    }
+
+    return originalRender(React.createElement(tags[component], props));
 }
 
 export {fireEvent, wait, waitForElement};
