937e7745fc465e094b6ebd712774f5e4549e6a99
alex <alex@activewidgets.com>
2020-01-29 00:30:09 +0100

    upgrade examples

--- examples/columns/src/index.js
+++ examples/columns/src/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Datagrid } from "@activewidgets/react";
-import * as data from "@activewidgets/examples/northwind";
+import { northwind } from "@activewidgets/examples/data";
 
 const columns = [
-  {header: 'Code', field: 'CustomerID', width: 80, style: 'background:#def', fixed: true},
-  {header: 'Company Name', field: 'CompanyName', width: 160},
-  {header: 'Contact', field: 'ContactName', width: 120},
-  {header: 'Title', field: 'ContactTitle', width: 120},
-  {header: 'Address', field: 'Address', width: 120},
-  {header: 'City', field: 'City'},
-  {header: 'Zip', field: 'PostalCode', align: 'right'},
-  {header: 'Phone', field: 'Phone'},
-  {header: 'Fax', field: 'Fax'},
-  {header: 'Country', field: 'Country'}
+  {header: 'Code', field: 'customerID', width: 80, style: 'background:#def', fixed: true},
+  {header: 'Company Name', field: 'companyName', width: 160},
+  {header: 'Contact', field: 'contactName', width: 120},
+  {header: 'Title', field: 'contactTitle', width: 120},
+  {header: 'Address', field: 'address', width: 120},
+  {header: 'City', field: 'city'},
+  {header: 'Zip', field: 'postalCode', align: 'right'},
+  {header: 'Phone', field: 'phone'},
+  {header: 'Fax', field: 'fax'},
+  {header: 'Country', field: 'country'}
 ];
 
 const App = () => (
-    <Datagrid columns={columns} rows={data.customers} />
+    <Datagrid columns={columns} rows={northwind.customers} />
 );
 
 ReactDOM.render(<App />, document.getElementById("app"));
