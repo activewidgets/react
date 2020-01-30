28a8ff1ee2923e71c45c97b31e348567d890a24c
alex <alex@activewidgets.com>
2020-01-30 22:59:19 +0100

    typo

--- examples/cdn-es5/src/index.js
+++ examples/cdn-es5/src/index.js
@@ -2,7 +2,7 @@
 var Datagrid = ActiveWidgets.React.Datagrid;
 
 var rows = [
-    { framework: 'React', source: 'CDN', landuage: 'ES5'}
+    { framework: 'React', source: 'CDN', language: 'ES5'}
 ];
 
 var App = React.createElement(Datagrid, {rows: rows});
